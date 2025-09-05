62b8e2fa4c3fb92ec589333376a7da6f8d26a331
Pumpinator <alejandrodcardona5@gmail.com>
2025-09-05 10:51:53 -0600

    feat: se oculto info y se borro un archivo inutil

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   baseUrl: 'http://localhost:3000',
   useMocks: true,
   featureFlags: { charts: true },
-  banxicoToken: '1b6140b01d197c5e23dc49dfb5a68b32b5cad7ba7e8a08425627132eaef0d2aa'
+  banxicoToken: ''
 };
